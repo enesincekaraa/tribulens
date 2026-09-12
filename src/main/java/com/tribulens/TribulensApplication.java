e29239f96d6f0c7d673e665f1d348c7c5c9d59e3
Enes Incekara <enesincekara61@gmail.com>
2026-09-12 13:11:06 +0300

    chore: initialize Tribulens Spring Boot application

---
+++ src/main/java/com/tribulens/TribulensApplication.java
@@ -0,0 +1,13 @@
+package com.tribulens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TribulensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TribulensApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tribulens/TribulensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tribulens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TribulensApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
